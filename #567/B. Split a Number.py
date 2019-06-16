39be534288aef66206cf8193da3cd8a3d35e16eb
Hybrid Skills <ankurgoyal616@gmail.com>
2019-06-16 16:56:14 +0530

    mid exam commit

--- #567/B. Split a Number.py
+++ #567/B. Split a Number.py
@@ -14,14 +14,20 @@ if l % 2 == 0:
 else:
     str_x_left = str(n)[0:l // 2]
     str_y_left = str(n)[l // 2:l]
-    str_x_right = str(n)[0:(l//2) +1]
+    str_x_right = str(n)[0:(l//2) + 1]
     str_y_right = str(n)[(l//2)+1:l]
 
-while str_y_right[0] == "0" and str_y_left[0] == "0":
-    str_x_right += "0"
-    str_y_right = str_y_right[1:]
-    str_y_left = str_x_left[-1] + str_y_left
-    str_x_left = str_x_left[:-1]
+i=1
+j=0
+
+while str_y_right[j] == "0" and str_x_left[-i] == "0":
+    j += 1
+    i += 1
+
+str_x_right += "0"*j
+str_y_right = str_y_right[j:]
+str_y_left = str_x_left[-i:] + str_y_left
+str_x_left = str_x_left[:-i]
 
 sum_moving_right = int(str_x_right) + int(str_y_right)
 
