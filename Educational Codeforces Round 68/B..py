5a2bcb8b21383bbc591530954a06067e57752aca
Hybrid Skills <ankurgoyal616@gmail.com>
2019-07-15 21:07:40 +0530

    Educational Round 68: A, B, C, D are correct

--- Educational Codeforces Round 68/B..py
+++ Educational Codeforces Round 68/B..py
@@ -1,29 +1,40 @@
 q = int(input())
-minimum = minimum_column = 46 * 10 ** 6
+
 
 for _ in range(q):
-    n, m = [int(i) for i in input().split()]
+    max1 = 0
+    max2 = 0
     rows = []
-    for __ in range(n):
-        rows.append(input())
-        total = 0
-        for i in rows[-1]:
-            total += ord(i)
-        minimum = min(total, minimum)
+    pos1 = []
+    pos2 = []
+    n, m = map(int, input().split())
+    for i in range(n):
+        row = input()
+        count = row.count('*')
+        rows.append(row)
+        if max1 == count:
+            pos1.append(i)
+        elif max1 < count:
+            pos1 = [i]
+            max1 = count
+    for i in range(m):
+        count = 0
+        for j in range(n):
+            if rows[j][i] == '*':
+                count += 1
+        if max2 == count:
+            pos2.append(i)
+        elif max2 < count:
+            pos2 = [i]
+            max2 = count
 
-    minutes_rows = (minimum - (m * 42)) //4
-
-    print(minutes_rows)
-    # columns = []
-    # for i in range(m):
-    #     columns.append([])
-    # for i in range(m):
-    #     for j in range(n):
-    #         columns[i].append(rows[j][i])
-    #     total_column = 0
-    #     for j in columns[i]:
-    #         total_column += ord(j)
-    #     minimum_column = min(total_column, minimum_column)
-    #
-    # minutes = minutes_rows + (minimum_column - (n-42)) / 4
-    # print(minutes)
+    for i in pos1:
+        for j in pos2:
+            if rows[i][j] == '.':
+                print((n - max1) + (m - max2) - 1)
+                break
+        else:
+            continue
+        break
+    else:
+        print((m - max1) + (n - max2))
