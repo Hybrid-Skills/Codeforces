9f0e7977a625b0f92770f02d803060c7749d0f65
Hybrid Skills <ankurgoyal616@gmail.com>
2019-07-16 00:01:53 +0530

    #2 A is right

--- #2/A. Winner.py
+++ #2/A. Winner.py
@@ -1,37 +1,31 @@
-# Question A. Winner
-
-# The winner of the card game popular in Berland "Berlogging" is determined
-# according to the following rules. If at the end of the game there is only one
-# player with the maximum number of points, he is the winner. The situation
-# becomes more difficult if the number of such players is more than one. During
-# each round a player gains or loses a particular number of points. In the
-# course of the game the number of points is registered in the line "name
-# score", where name is a player's name, and score is the number of points
-# gained in this round, which is an integer number. If score is negative, this
-# means that the player has lost in the round. So, if two or more players have
-# the maximum number of points (say, it equals to m) at the end of the game,
-# than wins the one of them who scored at least m points first. Initially each
-# player has 0 points. It's guaranteed that at the end of the game at least one
-# player has a positive number of points.
-
-
 n = int(input())
-scores = []
-num = []
-maximum = 0
+result = []
+score_sum = {}
+score_sum1 = {}
 
 for _ in range(n):
-    q = input().split()
-    if q[0] in scores:
-        scores[scores.index(q[0]) + 1] += int(q[1])
-    else:
-        scores = scores + [q[0], int(q[1])]
-    if scores[scores.index(q[0]) + 1] > maximum:
-        maximum = scores[scores.index(q[0]) + 1]
-        winner = q[0]
-for i in scores:
-    if i is int:
-        num.append(i)
+    result.append(input().split())
 
+for i in result:
+    try:
+        score_sum[i[0]] += int(i[1])
+    except KeyError:
+        score_sum[i[0]] = int(i[1])
 
-print(winner)
+winning_score = max(score_sum.values())
+
+possible_winners = [key for (key, value) in score_sum.items() if value == winning_score]
+
+if len(possible_winners) == 1:
+    print(possible_winners[0])
+
+else:
+    for i in result:
+        if i[0] in possible_winners:
+            try:
+                score_sum1[i[0]] += int(i[1])
+            except KeyError:
+                score_sum1[i[0]] = int(i[1])
+            if score_sum1[i[0]] >= winning_score:
+                print(i[0])
+                break
