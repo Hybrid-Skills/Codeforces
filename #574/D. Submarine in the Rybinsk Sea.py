4f30ede8ab0955656dd063017922c17603595550
Hybrid Skills <ankurgoyal616@gmail.com>
2019-07-20 13:05:09 +0530

    #574 A, B, C, D1, D2 are correct

--- #574/D. Submarine in the Rybinsk Sea.py
+++ #574/D. Submarine in the Rybinsk Sea.py
@@ -3,21 +3,39 @@ n = int(input())
 question = input().split()
 
 number = 0
-carry = 0
 
 for i in range(n):
-    question[i] = list(map(int,list(question[i])))
+    question[i] = list(map(int, list(question[i])))
 
-transposed = list(zip(*question))
+j = 0
+transposed = [[], [], [], [], [], [], [], [], [], []]
+while j <= 9:
+    for i in range(n):
+        try:
+            transposed[j].append(question[i][-j-1])
+        except IndexError:
+            pass
+    j += 1
 
-for i in range(1, len(question[0])+1):
-    query = n*sum(transposed[-i]) + carry
-    number += (10**(2*i-2))*(query % 10)
-    carry = query//10
-    query = n * sum(transposed[-i]) + carry
-    number += (10 ** (2*i-1)) * (query % 10)
-    carry = query // 10
 
-number += 10**(2*len(question[0])) * carry
+max_digits = 0
+for i in range(10):
+    if not transposed[i]:
+        max_digits = i
+        break
+else:
+    max_digits = 10
+
+for i in range(max_digits):
+    temp_sum = sum(transposed[i])
+
+    number += (10 ** (2*i)) * 11 * len(transposed[i]) * temp_sum
+
+    if i > 0:
+        j = 1
+        while j <= 9:
+            if i >= j:
+                number += ((10**(2*i + 1 - j)) * (2 * (len(transposed[i-j]) - len(transposed[i-j+1])))*temp_sum)
+            j += 1
 
 print(number % 998244353)
