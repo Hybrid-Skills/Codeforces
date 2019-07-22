e83c44d57bec98f0a6bbc734c2c3a1d286c489f4
Hybrid Skills <ankurgoyal616@gmail.com>
2019-07-22 17:49:01 +0530

    Global Round 4: A, B, C, D, E are correct

--- Global Round 4/E. Archaeology.py
+++ Global Round 4/E. Archaeology.py
@@ -1,14 +1,35 @@
 s = input()
+n = len(s)
+# x = 0
+# y = n-1
+# ans = ''
+# flag = False
+#
+# while x < y - 2:
+#     if s[x] == s[y] or s[x] == s[y-1]:
+#         ans += s[x]
+#     else:
+#         ans += s[x+1]
+#     x += 2
+#     y -= 2
+#
+# if y - x >= 1:
+#     print(ans + s[x] + ans[::-1])
+# else:
+#     print(ans + ans[::-1])
 
-i = 0
-j = -1
-part1 = ''
-part2 = ''
-
-
-while i < len(s) and j >= -len(s):
-    if s[i] == s[j]:
-        part1 += s[i]
-        part2 = s[i] + part2
+i, j = 0, n - 1
+ans = []
+t = n // 4
+for i in range(t):
+    x = i*2
+    y = n - 2 - i * 2
+    if s[x] == s[y] or s[x] == s[y+1]:
+        ans.append(s[x])
     else:
-        for x in range(len(s)):
+        ans.append(s[x+1])
+if n % 4 >= 2:
+    ans = ans + [s[n // 2]] + ans[::-1]
+else:
+    ans = ans+ans[::-1]
+print(''.join(ans))
