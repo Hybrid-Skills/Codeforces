ff475a4aa1d5dde7d614d4a6dfc8285a4911e557
Hybrid Skills <ankurgoyal616@gmail.com>
2019-09-30 21:19:58 +0530

    A, B, C are correct

--- #589/D. Complete Tripartite.py
+++ #589/D. Complete Tripartite.py
@@ -1,17 +1,57 @@
 n, m = map(int, input().split())
-v = [0 for i in range(n)]
-gr = [[] for i in range(n)]
+v = [2 for i in range(n)]
+x = [False for i in range(n)]
+gr = [[] for j in range(n)]
 
 for i in range(m):
     x, y = map(int, input().split())
-    gr[x].append(y)
-    gr[y].append(x)
+    gr[x-1].append(y-1)
+    gr[y-1].append(x-1)
 
-v[0] = 1
-x = gr[0].pop()
-v[x] = 2
-y = gr[x].pop()
-i = 2
+for i in gr:
+    if len(i) < 2:
+        print(-1)
+        quit()
+# print(gr)
 
-while True:
-    if y in gr[x]:
+for i in gr[0]:
+    x[i] = True
+
+for i in range(n):
+    if not x[i]:
+        v[i] = 0
+
+for i in gr[gr[0][0]]:
+    if x[i]:
+        v[i] = 1
+
+v1 = 0
+v0 = 0
+
+for i in v:
+    if i == 0:
+        v0 += 1
+    elif i == 1:
+        v1 += 1
+
+v2 = n - v0 - v1
+
+if v1 == 0 or v2 == 0 or v0 == 0:
+    print(-1)
+    quit()
+
+for i in range(n):
+    if (v[i] == 0 and len(gr[i]) != v1 + v2) or \
+            (v[i] == 1 and len(gr[i]) != v0+ v2) or \
+            (v[i] == 2 and len(gr[i]) != v1 + v0):
+        print(-1)
+        quit()
+#
+# for i in range(n):
+#     for j in gr[i]:
+#         if v[i] == v[j]:
+#             print(-1)
+#             quit()
+
+for i in v:
+    print(i+1, end=' ')
